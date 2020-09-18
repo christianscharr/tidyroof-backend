f5a1898d61ae6461849138befbaaa41e63aa33b7
xXTime-OnXx <timon.schmid@bluewin.ch>
2020-09-18 23:05:52 +0200

    updated LoggingInterceptor

--- src/logging.interceptor.ts
+++ src/logging.interceptor.ts
@@ -1,6 +1,6 @@
 import {CallHandler, ExecutionContext, Injectable, Logger, NestInterceptor} from '@nestjs/common';
-import {Observable} from 'rxjs';
-import {tap} from 'rxjs/operators';
+import {Observable, throwError} from 'rxjs';
+import {catchError, tap} from 'rxjs/operators';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
@@ -17,6 +17,10 @@ export class LoggingInterceptor implements NestInterceptor {
 			.handle()
 			.pipe(
 				tap((data) => this.logger.debug(`Received message controller [${context.getClass().name}.${context.getHandler().name}] | status: ${res.statusCode} | duration: ${Date.now() - now}ms | { data: ${JSON.stringify(data)} }`)),
+				catchError((err) => {
+					this.logger.error(`Error thrown in [${context.getClass().name}.${context.getHandler().name}] | ${JSON.stringify(err)}`, err.trace);
+					return throwError(err);
+				}),
 			);
 	}
 }
