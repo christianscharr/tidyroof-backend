f3c5e0e65f7618e27902aaa48bc54309be4d61a6
xXTime-OnXx <timon.schmid@bluewin.ch>
2020-09-18 23:09:14 +0200

    new project structure

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import {Logger} from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {LoggingInterceptor} from './logging.interceptor';
+import {LoggingInterceptor} from './interceptor/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
