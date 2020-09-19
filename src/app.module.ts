b6dfb3b1011b7f6e25d22fbb4e4e0f4bab305b5a
Christian Scharr <christian.scharr@gmail.com>
2020-09-19 18:28:33 +0200

    stub of prediction backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,19 @@
-import {HttpModule, Module} from '@nestjs/common';
+import { HttpModule, Module } from '@nestjs/common';
 import { AppController } from './controller/app/app.controller';
 import { ProductController } from './controller/product/product.controller';
-import {AppService} from "./service/app.service";
-import {ProductService} from "./service/product.service";
+import { AppService } from './service/app.service';
+import { ProductService } from './service/product.service';
+import { PredictionService } from './service/prediction.service';
 
 @Module({
-    imports: [HttpModule.registerAsync({
-        useFactory: () => ({
-            timeout: 10000,
-        }),
-    })
-    ],
-    controllers: [AppController, ProductController],
-    providers: [AppService, ProductService],
+  imports: [HttpModule.registerAsync({
+    useFactory: () => ({
+      timeout: 10000,
+    }),
+  }),
+  ],
+  controllers: [AppController, ProductController],
+  providers: [AppService, ProductService, PredictionService],
 })
-export class AppModule {}
+export class AppModule {
+}
